43b48979c3735cb7044a2c2bbd13350f6c21f895
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2020-07-05 20:41:18 -0700

    using dictionary on two sum (1)

--- 1.py
+++ 1.py
@@ -1,7 +1,19 @@
+#7/5/20 Using Dictionary
 class Solution(object):
     def twoSum(self, nums, target):
+        numbers = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if(nums[j] == target - nums[i]):
-                    return [i,j]           
-        print("No Solution")
+            comp = target - nums[i]
+            if comp in numbers:
+                return [numbers[comp], i]
+            numbers[nums[i]] = i
+
+# Brute Force Solution - 7/5/2020
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if(nums[j] == target - nums[i]):
+#                     return [i,j]           
+#         print("No Solution")

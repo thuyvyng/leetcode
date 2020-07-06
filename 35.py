8f55a1428b6787370013cc8888e7856f88c59fc5
Thuy-Vy Nguyen <thuyvynguy@gmail.com>
2020-07-06 13:46:03 -0700

    update 35 by adding binary search

--- 35.py
+++ 35.py
@@ -1,10 +1,52 @@
+# Using Binary Search
 class Solution(object):
     def searchInsert(self, nums, target):
+        if target < nums[0]:
+            return 0
+        if target > nums[len(nums)-1]:
+            return len(nums)
+        
         try:
             return nums.index(target)
         except:
-            for x in range(len(nums)):
-                if (nums[x] >= target):
-                    return x
-            return len(nums)
+            return self.binary_search(nums,target)
+            
+        
+    def binary_search(self,arr,target): 
+        low = 0
+        high = len(arr) - 1
+        mid = 0
+
+        while low <= high: 
+
+            mid = (high + low) // 2
+
+            # Check if x is present at mid 
+            if arr[mid] < target: 
+                low = mid + 1
+
+            # If x is greater, ignore left half 
+            elif arr[mid] > target: 
+                high = mid - 1
+
+            # If x is smaller, ignore right half 
+            else: 
+                return mid 
+
+        # If we reach here, then the element was not present
+        if target > arr[low]:
+            return low + 1
+        else:
+            return low
+
+# Loop through array
+# class Solution(object):
+#     def searchInsert(self, nums, target):
+#         try:
+#             return nums.index(target)
+#         except:
+#             for x in range(len(nums)):
+#                 if (nums[x] >= target):
+#                     return x
+#             return len(nums)
         
